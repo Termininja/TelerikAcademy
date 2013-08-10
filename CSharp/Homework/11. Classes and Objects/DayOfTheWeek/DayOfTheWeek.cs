566c59552258de7c56e831142c5cc417200251f6
Termininja <slavi.bg@gbg.bg>
2013-08-10 22:50:43 +0300

    CSharp

---
+++ CSharp/Homework/11. Classes and Objects/DayOfTheWeek/DayOfTheWeek.cs
@@ -0,0 +1,15 @@
+﻿//Task3: Write a program that prints to the console which day of the week is today.
+//       Use System.DateTime.
+
+using System;
+
+class DayOfTheWeek
+{
+    static void Main()
+    {
+        Console.Write("Today is: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine((DateTime.Now).DayOfWeek);        // prints the day of the week
+        Console.ResetColor();
+    }
+}
---
+++ CSharp/Homework/11. Classes and Objects/RandomValues/RandomValues.cs
@@ -0,0 +1,24 @@
+﻿//Task2: Write a program that generates and prints to the console 10 random values in the range [100, 200].
+
+using System;
+using System.Threading;
+
+class RandomValues
+{
+    static void Main()
+    {
+        Random RandomGenerator = new Random();                      // sets a random generator
+        for (int i = 0; i < 10; i++)                                // for each 10 values
+        {
+            for (int j = 0; j < 100; j++)                           // takes the last from 99 times
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("Random value {0:00}: ", i + 1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(RandomGenerator.Next(100, 201));  // prints the result
+                Console.ResetColor();
+                Thread.Sleep(20);                                   // sleep the program for 20ms
+            }
+        }
+    }
+}
