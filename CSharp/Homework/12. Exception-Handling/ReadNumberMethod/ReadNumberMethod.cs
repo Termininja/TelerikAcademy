09a9ad57c74c3c989f0310cd0f448486c3d05da6
Termininja <slavi.bg@gbg.bg>
2013-08-21 00:18:30 +0300

    CSharp

---
+++ CSharp/Homework/12. Exception-Handling/ReadNumberMethod/ReadNumberMethod.cs
@@ -0,0 +1,31 @@
+﻿//Task2: Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end].
+//       If an invalid number or non-number text is entered, the method should throw an exception.
+//       Using this method write a program that enters 10 numbers: a1, a2, … a10, such that 1 < a1 < … < a10 < 100
+
+using System;
+
+class ReadNumberMethod
+{
+    static int a;
+    static void Main()
+    {
+        int start = 1;                                  // the minimum number is 1
+        for (a = 1; a <= 10; a++)                       // for each 10 numbers
+        {
+            start = ReadNumber(start, 100);             // calls the ReadNumber method
+        }
+    }
+
+    static int ReadNumber(int start, int end)
+    {
+        Console.Write("Please, enter some number ({0} < a{2} < {1}): ", start, end, a);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        int number = int.Parse(Console.ReadLine());     // reads some integer number
+        Console.ResetColor();
+        if (!(start < number && number < end))          // is the number in our range
+        {
+            throw new ArgumentOutOfRangeException();    // throw Exception if the number is out of our range
+        }
+        return number;
+    }
+}
---
+++ CSharp/Homework/12. Exception-Handling/SquareRoot/SquareRoot.cs
@@ -0,0 +1,43 @@
+﻿//Task1: Write a program that reads an integer number and calculates and prints its square root.
+//       If the number is invalid or negative, print "Invalid number".
+//       In all cases finally print "Good bye". Use try-catch-finally.
+
+using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Please, enter some integer number: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int number = int.Parse(Console.ReadLine());     // reads some integer number
+            Console.ResetColor();
+            if (number < 0)                                 // throw exception if the number is negative
+            {
+                throw new FormatException();
+            }
+            Console.Write("The square root of {0} is: ", number);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Math.Sqrt(number));           // prints the result from Sqrt()
+            Console.ResetColor();
+        }
+        catch (FormatException)                             // when there is "FormatException"
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid number");
+
+        }
+        catch (OverflowException)                           // when the number is too big
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Too big number!");
+        }
+        finally                                             // finally is printed "Good bye"
+        {
+            Console.ResetColor();
+            Console.WriteLine("\nGood bye");
+        }
+    }
+}
