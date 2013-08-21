4e9e30da7f01f6d37ab8c4e7539d6247ae849b03
Termininja <slavi.bg@gbg.bg>
2013-08-21 20:47:05 +0300

    CSharp

---
+++ CSharp/Homework/01. Introduction to Programming/FamiliarizeWithVS/FamiliarizeWithVS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class FamiliarizeWithVS
+{
+    static void Main()
+    {
+        Console.Write("Task: ");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Familiarize yourself with Microsoft Visual Studio and MSDN Library Documentation");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Decision: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Done!\n");
+        Console.ResetColor();
+    }
+}
---
+++ CSharp/Homework/01. Introduction to Programming/InstallVS/InstallVS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class InstallVS
+{
+    static void Main()
+    {
+        Console.Write("Task: ");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Install at home Microsoft Visual Studio");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\nDecision: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Done!\n");
+        Console.ResetColor();
+    }
+}
