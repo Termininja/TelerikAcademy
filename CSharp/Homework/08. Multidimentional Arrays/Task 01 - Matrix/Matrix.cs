a6a0eb8d739f0adb06a10c1e7cd4138b34168047
Termininja <slavi.bg@gbg.bg>
2013-09-25 21:58:59 +0300

    CSharp

--- CSharp/Homework/08. Multidimentional Arrays/Task 01 - Matrix/Matrix.cs
+++ CSharp/Homework/08. Multidimentional Arrays/Task 01 - Matrix/Matrix.cs
@@ -12,17 +12,17 @@ class Matrix
     static void Main()
     {
         Console.Write("Please, enter the number: n = ");
-        int N = int.Parse(Console.ReadLine());
-        int[,] M = new int[N, N];
+        int N = int.Parse(Console.ReadLine());                  // reads some number N
+        int[,] M = new int[N, N];                               // creates one matrix NxN       
 
         Console.WriteLine();
-        PrintMatrix(VariantA(N, M));
+        PrintMatrix(VariantA(N, M));                            // prints the result for variant A
         Console.WriteLine();
-        PrintMatrix(VariantB(N, M));
+        PrintMatrix(VariantB(N, M));                            // prints the result for variant B
         Console.WriteLine();
-        PrintMatrix(VariantC(N, M));
+        PrintMatrix(VariantC(N, M));                            // prints the result for variant C
         Console.WriteLine();
-        PrintMatrix(VariantD(N, M));
+        PrintMatrix(VariantD(N, M));                            // prints the result for variant D
     }
 
     static int[,] VariantA(int N, int[,] M)
@@ -30,11 +30,11 @@ class Matrix
         int col = -1;
         for (int n = 0; n < N * N; n++)
         {
-            if (n % N == 0)
+            if (n % N == 0)                                     // go to the next column
             {
                 col++;
             }
-            M[n % N, col] = n + 1;
+            M[n % N, col] = n + 1;                              // the row depends on current N
         }
         return M;
     }
@@ -45,11 +45,11 @@ class Matrix
         int row = 0;
         for (int n = 0; n < N * N; n++)
         {
-            if (n % N == 0)
+            if (n % N == 0)                                     // go to the next column 
             {
                 col++;
             }
-            if (n % N != 0)
+            if (n % N != 0)                                     // calculates the current row
             {
                 row = col % 2 == 0 ? (row + 1) % N : (row - 1) % N;
             }
@@ -63,20 +63,20 @@ class Matrix
     {
         int col = 0;
         int row = N - 1;
-        int specialPos = N - 1;
+        int specialPos = N - 1;                                 // the new start position
         for (int n = 1; n <= N * N; n++)
         {
             M[row, col] = n;
-            if (row == N - 1 || col == N - 1)
+            if (row == N - 1 || col == N - 1)                   // the limit of matrix
             {
                 specialPos--;
-                col = specialPos >= 0 ? 0 : -specialPos;
-                row = specialPos >= 0 ? specialPos : 0;
+                col = specialPos >= 0 ? 0 : -specialPos;        // go to new column
+                row = specialPos >= 0 ? specialPos : 0;         // go to new row
             }
             else
             {
-                col++;
-                row++;
+                col++;                                          // calculates the current column
+                row++;                                          // calculates the current row
             }
         }
         return M;
@@ -86,37 +86,37 @@ class Matrix
     {
         int col = 0;
         int row = 0;
-        int rowMin = 0;
-        int rowMax = N - 1;
-        int colMin = 1;
-        int colMax = N - 1;
+        int rowMin = 0;                                         // minimum rows for spiral
+        int rowMax = N - 1;                                     // the limit of rows for spiral
+        int colMin = 1;                                         // minimim columns for spiral
+        int colMax = N - 1;                                     // the limit of columns for spiral
         char dir = 'd';
 
         for (int n = 1; n <= N * N; n++)
         {
             M[row, col] = n;
 
-            switch (dir)
+            switch (dir)                                        // checks the current direction
             {
-                case 'd': row++;
+                case 'd': row++;                                // if direction is "down"
                     if (row > rowMax)
                     {
                         row--; col++; rowMax--; dir = 'r';
                     }
                     break;
-                case 'r': col++;
+                case 'r': col++;                                // if direction is "right"
                     if (col > colMax)
                     {
                         col--; row--; colMax--; dir = 'u';
                     }
                     break;
-                case 'u': row--;
+                case 'u': row--;                                // if direction is "up"
                     if (row < rowMin)
                     {
                         col--; row++; rowMin++; dir = 'l';
                     }
                     break;
-                case 'l': col--;
+                case 'l': col--;                                // if direction is "left"
                     if (col < colMin)
                     {
                         col++; row++; colMin++; dir = 'd';
@@ -130,7 +130,7 @@ class Matrix
 
     static void PrintMatrix(int[,] M)
     {
-        for (int i = 0; i < M.GetLength(0); i++)
+        for (int i = 0; i < M.GetLength(0); i++)                    // prints the matrix
         {
             for (int j = 0; j < M.GetLength(1); j++)
             {
---
+++ CSharp/Homework/08. Multidimentional Arrays/Task 03 - SequencesInMatrix/SequencesInMatrix.cs
@@ -0,0 +1,82 @@
+﻿//Task 3: We are given a matrix of strings of size N x M. Sequences in the matrix we define
+//        as sets of several neighbor elements located on the same line, column or diagonal.
+//        Write a program that finds the longest sequence of equal strings in the matrix. Example:
+//
+//        ha  fifi  ho  hi                            s qq s    
+//        fo  ha    hi  xx   ->  ha,ha,ha            pp pp s    -> s,s,s
+//        xxx ho    ha  xx                           pp qq s    
+
+using System;
+
+class SequencesInMatrix
+{
+    static int maxLength = 0;                                       // the value of longest sequence of equal strings
+    static string maxWord = String.Empty;                           // the string of longest sequence
+
+    static void Main()
+    {
+        string[,] Matrix = { { "ha", "fifi", "ho", "hi" }, { "fo", "ha", "hi", "xx" }, { "xxx", "ho", "ha", "xx" } };
+        for (int row = 0; row < Matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < Matrix.GetLength(1); col++)     //check for all elements in the matrix
+            {
+                string currWord = Matrix[row, col];
+                int currLengthRow = 1;                              // the maximum length of sequence by row
+                int currLengthCol = 1;                              // the maximum length of sequence by column
+                int currLengthDiag1 = 1;                            // the maximum length of sequence by diagonal 1 (right-top)
+                int currLengthDiag2 = 1;                            // the maximum length of sequence by diagonal 2 (right-down)
+                while (row > 0 && row < Matrix.GetLength(0) && Matrix[row, col] == Matrix[row - 1, col])
+                {
+                    currLengthRow++;                                // the lenght of current sequence by row
+                    row++;
+                }
+                while (col > 0 && col < Matrix.GetLength(1) && Matrix[row, col] == Matrix[row, col - 1])
+                {
+                    currLengthCol++;                                // the lenght of current sequence by column
+                    col++;
+                }
+                while (col > 0 && col < Matrix.GetLength(1) && row > 0 && row < Matrix.GetLength(0) && Matrix[row, col] == Matrix[row + 1, col - 1])
+                {
+                    currLengthDiag1++;                              // the lenght of current sequence by diagonal 1
+                    col++;
+                    row--;
+                }
+                while (col > 0 && col < Matrix.GetLength(1) && row > 0 && row < Matrix.GetLength(0) && Matrix[row, col] == Matrix[row - 1, col - 1])
+                {
+                    currLengthDiag2++;                              // the lenght of current sequence by diagonal 2
+                    col++;                                          
+                    row++;
+                }
+
+                CheckMax(currWord, currLengthRow);                  // is the current length by row bigger than maximal value
+                CheckMax(currWord, currLengthCol);                  // is the current length by column bigger than maximal value
+                CheckMax(currWord, currLengthDiag1);                // is the current length by diagonal 1 bigger than maximal value
+                CheckMax(currWord, currLengthDiag2);                // is the current length by diagonal 2 bigger than maximal value
+            }
+        }
+
+        PrintSequence(maxLength, maxWord);                          // prints the result
+    }
+
+    private static void CheckMax(string currWord, int currLen)
+    {
+        if (currLen >= maxLength)
+        {
+            maxLength = currLen;
+            maxWord = currWord;
+        }
+    }
+
+    private static void PrintSequence(int len, string word)
+    {
+        for (int i = 0; i < len; i++)                               // prints the max sequence
+        {
+            Console.Write(word);
+            if (i != len - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
