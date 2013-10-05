9b737a18ed46327274533d129e893ed5ce00708d
Termininja <slavi.bg@gbg.bg>
2013-10-05 15:51:53 +0300

    CSharp

--- CSharp/Homework/13. Text Files/Task 01 - PrintOddLines/PrintOddLines.cs
+++ CSharp/Homework/13. Text Files/Task 01 - PrintOddLines/PrintOddLines.cs
@@ -7,17 +7,14 @@ class PrintOddLines
 {
     static void Main()
     {
-        StreamReader read = new StreamReader("file.txt");         	// reads some text file
+        StreamReader read = new StreamReader("file.txt");       // reads some text file
         using (read)
         {
-            for (int line = 1; ; line++)
+            string text = read.ReadLine();
+            for (int line = 1; text != null; line++)
             {
-                string text = read.ReadLine();                      // reads the current line
-                if (text == null) break;                            // stop in the end of the file
-                if (line % 2 != 0)
-                {
-                    Console.WriteLine(text);                        // prints only odd lines
-                }
+                if (line % 2 != 0) Console.WriteLine(text);     // prints only odd lines
+                text = read.ReadLine();                         // reads the current line
             }
         }
     }
