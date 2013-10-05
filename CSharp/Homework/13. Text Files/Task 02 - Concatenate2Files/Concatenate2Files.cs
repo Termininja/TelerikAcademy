590eed10c230e40afb02f015dc5eefbe8f377c70
Termininja <slavi.bg@gbg.bg>
2013-10-05 15:54:14 +0300

    CSharp

---
+++ CSharp/Homework/13. Text Files/Task 02 - Concatenate2Files/Concatenate2Files.cs
@@ -0,0 +1,21 @@
+﻿//Task2: Write a program that concatenates two text files into another text file.
+
+using System;
+using System.IO;
+using System.Text;
+
+class Concatenates2Files
+{
+    static void Main()
+    {
+        StreamWriter write = new StreamWriter("result.txt");        // the output file 
+        using (write)
+        {
+            StreamReader file1 = new StreamReader("file1.txt");     // read some text file 1
+            using (file1) write.WriteLine(file1.ReadToEnd());       // write file 1 to output file
+
+            StreamReader file2 = new StreamReader("file2.txt");     // read some text file 2
+            using (file2) write.WriteLine(file2.ReadToEnd());       // write file 2 to output file
+        }
+    }
+}
