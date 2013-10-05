552dc90a4bf96905298c1282bc009b0deb5472f4
Termininja <slavi.bg@gbg.bg>
2013-10-05 16:52:08 +0300

    CSharp

---
+++ CSharp/Homework/13. Text Files/Task 03 - InsertLine/InsertLine.cs
@@ -0,0 +1,26 @@
+﻿//Task3: Write a program that reads a text file and inserts line numbers in front of each of its lines.
+//       The result should be written to another text file.
+
+using System;
+using System.IO;
+
+class InsertLine
+{
+    static void Main()
+    {
+        StreamWriter write = new StreamWriter("result.txt");        // the name of output file
+        using (write)
+        {
+            StreamReader read = new StreamReader("file.txt");       // reads some text file
+            using (read)
+            {
+                string text = read.ReadLine();
+                for (int line = 1; text != null; line++)
+                {
+                    write.WriteLine(line + ": " + text);            // write the result in file
+                    text = read.ReadLine();                         // reads the current line
+                }
+            }
+        }
+    }
+}
