c2837b2ebae8d9ea34be58888bf8a3038dad95b5
Termininja <slavi.bg@gbg.bg>
2013-10-06 14:02:21 +0300

    CSharp

---
+++ CSharp/Homework/13. Text Files/Task 08 - ReplaceWholeWords/ReplaceWholeWords.cs
@@ -0,0 +1,29 @@
+﻿//Task8: Modify the solution of the previous problem to replace only whole words (not substrings).
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ReplaceWholeWords
+{
+    static void Main()
+    {
+        StringBuilder text = new StringBuilder();
+
+        StreamReader read = new StreamReader("file.txt");       // reads some text file
+        using (read)
+        {
+            // Imports the whole text file in one variable 'text'
+            text.Append(read.ReadToEnd());
+        }
+
+        // Write the replaced words in the same file
+        StreamWriter write = new StreamWriter("file.txt");
+        using (write)
+        {
+            // Replace only the words
+            write.WriteLine(Regex.Replace(text.ToString(), @"\bstart\b", "finish"));
+        }
+    }
+}
