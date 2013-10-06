78563fe0237913c5109ed19e254be1ad8170da36
Termininja <slavi.bg@gbg.bg>
2013-10-06 12:17:20 +0300

    CSharp

---
+++ CSharp/Homework/13. Text Files/Task 06 - ListOfStrings/ListOfStrings.cs
@@ -0,0 +1,39 @@
+﻿//Task6: Write a program that reads a text file containing a list of strings,
+//       sorts them and saves them to another text file. Example:
+//          Ivan			    George
+//      	Peter	    →		Ivan
+//      	Maria			    Maria
+//      	George			    Peter
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class ListOfStrings
+{
+    static void Main()
+    {
+        StreamReader read = new StreamReader("file.txt");           // reads some text file
+        using (read)
+        {
+            string line = read.ReadLine();
+            List<string> list = new List<string>();                 // creates empty list
+            for (int l = 0; line != null; l++)
+            {
+                // Adds each one word from the file in the list
+                list.Add(line);
+                line = read.ReadLine();                             // reads the next line
+            }
+            list.Sort();                                            // sorting the list
+            StreamWriter write = new StreamWriter("output.txt");
+            using (write)
+            {
+                // Write the sorted list in some output file
+                foreach (var word in list)      
+                {
+                    write.WriteLine(word);
+                }
+            }
+        }
+    }
+}
