70117d0a47e38ffb55e474f96e3a1994dfa03d2a
Termininja <slavi.bg@gbg.bg>
2013-10-31 21:59:19 +0200

    C#

--- C#/BGCoder/We All Love Bits!/We All Love Bits!.cs
+++ C#/BGCoder/We All Love Bits!/We All Love Bits!.cs
@@ -4,27 +4,26 @@ class WeAllLoveBits
 {
     static void Main()
     {
-        int N = int.Parse(Console.ReadLine());
-        for (int i = 0; i < N; i++)
+        int n = int.Parse(Console.ReadLine());
+        for (int j = 0; j < n; j++)
         {
-            int P = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
  
-            int P_inverted = P;
-            for (int k = 0; k < Convert.ToString(P_inverted, 2).Length; k++)
+            int inverted = p;
+            for (int k = 0; k < Convert.ToString(inverted, 2).Length; k++)
             {
-                P_inverted ^= (1 << k);
+                inverted ^= (1 << k);
             }
  
-            int P_reversed = 0;
-            int temp = P;
+            int reversed = 0;
+            int temp = p;
             while (temp > 0)
             {
-                P_reversed = (P_reversed << 1) | (temp & 1);
+                reversed = (reversed << 1) | (temp & 1);
                 temp = temp >> 1;
             }
  
-            int P_result = (P ^ P_inverted) & P_reversed;
-            Console.WriteLine(P_result);
+            Console.WriteLine((p ^ inverted) & reversed);
         }
     }
 }
