ee6b9709438c8434f8e25aa42a352609e6b43757
Termininja <slavi.bg@gbg.bg>
2013-11-09 15:55:10 +0200

    C#

--- C#/Homework/04. Console Input-Output/Task 09 - SequenceOfFibonacci/SequenceOfFibonacci.cs
+++ C#/Homework/04. Console Input-Output/Task 09 - SequenceOfFibonacci/SequenceOfFibonacci.cs
@@ -7,40 +7,20 @@ class SequenceOfFibonacci
 {
     static void Main()
     {
-        while (true)                                                            // repeat continuously
+        Console.Write("The first 100 members of the sequence of Fibonacci are: ");
+        ulong prev = 0;                             // the prvious member of the sequence
+        ulong curr = 0;                             // the current member of the sequence
+        ulong next = 1;                             // the next member of the sequence
+        for (int i = 1; i <= 100; i++)
         {
-            Console.Write("The first 100 members of the sequence of Fibonacci are: ");
-            decimal prev = 0;                                                   // the prvious member of the sequence
-            decimal curr = 0;                                                   // the current member of the sequence
-            decimal next = 1;                                                   // the next member of the sequence
-            for (int i = 1; i <= 100; i++)
-            {
-                System.Threading.Thread.Sleep(100);                             // the program will sleep for 100ms
-                prev = curr;
-                curr = next;
-                next = prev + curr;
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.Write(prev);                                            // this prints the all members of the sequence 
-                Console.ResetColor();
-                Console.Write(", ");
-            }
-            Console.Write("\b\b ");                                             // delete the last comma
-
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write("\n\nPress Enter to continue, or X to exit . . .");   // what we want to do
-        key:
-            ConsoleKeyInfo key = Console.ReadKey();
-            if (key.Key == ConsoleKey.X)                                        // you will exit if you press "X"
-            {
-                break;
-            }
-            else if (key.Key != ConsoleKey.Enter)                               // you will continue only when you press "Enter"
-            {
-                Console.Write("\b \b");                                         // clear the wrong keys from the console
-                goto key;
-            }
+            System.Threading.Thread.Sleep(100);     // the program will sleep for 100ms
+            prev = curr;
+            curr = next;
+            next = prev + curr;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(prev);                    // prints the all members of the sequence 
             Console.ResetColor();
-            Console.Clear();
+            if (i != 100) Console.Write(", ");
         }
     }
 }
