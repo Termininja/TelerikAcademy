cec7c2a433f836e927389389324a5aa0ead86e40
Termininja <slavi.bg@gbg.bg>
2013-12-04 18:02:10 +0200

    C#

--- C#/BGCoder/Garden/Garden.cs
+++ C#/BGCoder/Garden/Garden.cs
@@ -5,18 +5,16 @@ class Garden
     static void Main()
     {
         decimal[] prices = { 0.5m, 0.4m, 0.25m, 0.6m, 0.3m, 0.4m };
-
         decimal totalCost = 0;
         int beansArea = 250;
 
-        for (int v = 0; v < 6; v++)
+        for (int i = 0; i < 6; i++)
         {
-            totalCost += prices[v] * int.Parse(Console.ReadLine());
-            if (v < 5) beansArea -= int.Parse(Console.ReadLine());
+            totalCost += prices[i] * int.Parse(Console.ReadLine());
+            if (i < 5) beansArea -= int.Parse(Console.ReadLine());
         }
 
         Console.WriteLine("Total costs: {0:F2}", totalCost);
-
         if (beansArea < 0) Console.WriteLine("Insufficient area");
         else if (beansArea == 0) Console.WriteLine("No area for beans");
         else Console.WriteLine("Beans area: {0}", beansArea);
--- C#/BGCoder/Secrets/Secrets.cs
+++ C#/BGCoder/Secrets/Secrets.cs
@@ -7,38 +7,26 @@ class Secrets
     {
         BigInteger N = BigInteger.Parse(Console.ReadLine());
         BigInteger tempN = N;
-        int len = N.ToString().Length;
-        if (N < 0) len--;
-        BigInteger S = 0;
-        for (int i = 1; i <= len; i++)
+        if (N < 0) N = -1 * N;
+
+        BigInteger specialSum = 0;
+        int count = 0;
+        while (N > 0)
         {
-            BigInteger left;
-            if (N < 0) left = -(N % 10);
-            else left = N % 10;
-            if (i % 2 == 0) S += left * left * i;
-            else S += left * i * i;
-            N /= 10;
+            count++;
+            BigInteger n = N % 10;
+            specialSum += (count % 2 == 0) ? n * n * count : n * count * count;
+            N = N / 10;
         }
-        
-        BigInteger LastSum = S % 10;
-        Console.WriteLine(S);
-        if (LastSum == 0) Console.WriteLine("{0} has no secret alpha-sequence", tempN);
-        else
+        Console.WriteLine(specialSum);
+
+        string alphaSequence = String.Empty;
+        BigInteger first = 65 + specialSum % 26;
+        for (BigInteger i = first; i < first + specialSum % 10; i++)
         {
-            int k = 0;
-            int m = 1;
-            BigInteger R = S % 26;
-            for (int i = 0; i < LastSum; i++)
-            {
-                BigInteger temp = 65 + R + i;
-                Console.Write((char)(temp - k));
-                if (temp == 64 + 26 * m)
-                {
-                    k = 26;
-                    m++;
-                }
-            }
-            Console.WriteLine();
+            alphaSequence += (char)((i <= 90) ? i : (i - 65) % 26 + 65);
         }
+
+        Console.WriteLine((specialSum % 10 > 0) ? alphaSequence : tempN + " has no secret alpha-sequence");
     }
 }
