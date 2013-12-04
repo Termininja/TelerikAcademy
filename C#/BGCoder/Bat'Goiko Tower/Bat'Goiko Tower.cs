d547930ff565fcbafff88f823d634ec9ca27ca90
Termininja <slavi.bg@gbg.bg>
2013-12-04 21:38:34 +0200

    C#

--- C#/BGCoder/Bat'Goiko Tower/Bat'Goiko Tower.cs
+++ C#/BGCoder/Bat'Goiko Tower/Bat'Goiko Tower.cs
@@ -5,19 +5,18 @@ class BatGoikoTower
     static void Main()
     {
         int H = int.Parse(Console.ReadLine());
-        int temp = 1;
-        int k = 1;
-        for (int i = 0; i < H; i++)
+        int l = 1;
+        int d = 1;
+        for (int h = 1; h <= H; h++)
         {
-            Console.Write(new string('.', H - 1 - i) + "/");
-            if (i == temp)
+            Console.Write(new string('.', H - h) + "/");
+            Console.Write(new string((h == l + d) ? '-' : '.', 2 * (h - 1)));
+            if (h == l + d)
             {
-                Console.Write(new string('-', 2 * i));
-                k++;
-                temp = i + k;
+                l = h;
+                d++;
             }
-            else Console.Write(new string('.', 2 * i));
-            Console.Write("\\" + new string('.', H - 1 - i) + "\n");
+            Console.WriteLine("\\" + new string('.', H - h));
         }
     }
 }
