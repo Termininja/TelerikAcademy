56332ea214751f67694a529d4fc919fb0be87f01
Termininja <slavi.bg@gbg.bg>
2013-12-08 18:09:11 +0200

    OOP

---
+++ OOP/BGCoder/OOP Exam - Variant 1/AcademyRPG/Objects/Moving objects/Characters/Ninja.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class Ninja : Character, IFighter, IGatherer
+    {
+        private int attackPoints;
+
+        public Ninja(string name, Point position, int owner)
+            : base(name, position, owner)
+        {
+            this.HitPoints = 1;
+            this.attackPoints = 0;
+        }
+
+        public int AttackPoints
+        {
+            get { return this.attackPoints; }
+        }
+
+        public int DefensePoints
+        {
+            get { return int.MaxValue; }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            int highestHitPoints = 0;
+            int target = 0;
+            for (int i = 0; i < availableTargets.Count; i++)
+            {
+                if (availableTargets[i].Owner != this.Owner && availableTargets[i].Owner != 0)
+                {
+                    if (availableTargets[i].HitPoints > highestHitPoints)
+                    {
+                        target = i;
+                        highestHitPoints = availableTargets[i].HitPoints;
+                    }
+                }
+            }
+            return target;
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+                this.attackPoints += 2 * resource.Quantity;
+                return true;
+            }
+            if (resource.Type == ResourceType.Lumber)
+            {
+                this.attackPoints += resource.Quantity;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
