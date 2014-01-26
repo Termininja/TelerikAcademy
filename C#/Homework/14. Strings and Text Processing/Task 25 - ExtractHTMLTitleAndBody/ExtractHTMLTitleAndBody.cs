d65385eb95d66371b347da4e6144a3f80793836f
Termininja <slavi.bg@gbg.bg>
2014-01-26 20:39:46 +0200

    C#

--- C#/Homework/14. Strings and Text Processing/Task 25 - ExtractHTMLTitleAndBody/ExtractHTMLTitleAndBody.cs
+++ C#/Homework/14. Strings and Text Processing/Task 25 - ExtractHTMLTitleAndBody/ExtractHTMLTitleAndBody.cs
@@ -9,14 +9,18 @@
 //              </html>
 
 using System;
+using System.IO;
 using System.Text.RegularExpressions;
 
 class ExtractHTMLTitleAndBody
 {
     static void Main()
     {
-        string TextInHTML = @"<html><head><title>News</title></head>
-<body><p><a href=""http://academy.telerik.com"">Telerik Academy</a> aims to provide free real-world practical training...</p></body></html>";
-        Console.WriteLine(Regex.Replace(TextInHTML, @"<title>(.*)</title>|<a href="".*?"">(.*?)</a>|</?\w+>", m => m.Groups[1] + "" + m.Groups[2] + ""));
+        StreamReader reader = new StreamReader(@"..\..\file.html");
+        using (reader)
+        {
+            string TextInHTML = reader.ReadToEnd();
+            Console.WriteLine(Regex.Replace(TextInHTML, @"<title>(.*)</title>|<a href="".*?"">(.*?)</a>|</?\w+>", m => m.Groups[1] + "" + m.Groups[2] + ""));
+        }
     }
 }
