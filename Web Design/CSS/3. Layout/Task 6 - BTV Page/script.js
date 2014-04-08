c7f47a4507823a4638f5d5eb7016e2e0efc1f6f8
Termininja <slavi.bg@gbg.bg>
2014-04-08 19:12:10 +0300

    CSS

--- Web Design/CSS/3. Layout/Task 6 - BTV Page/script.js
+++ Web Design/CSS/3. Layout/Task 6 - BTV Page/script.js
@@ -3,19 +3,19 @@ window.onload = function () {
 }
 
 function MediaLink(link) {
-    var image = document.getElementById('image');
+    var image = document.getElementsByTagName('img');
     switch (link) {
-        case 1: image.src = "images/image1.jpg"; break;
-        case 2: image.src = "images/image2.jpg"; break;
-        case 3: image.src = "images/image3.jpg"; break;
-        case 4: image.src = "images/image4.jpg"; break;
-        case 5: image.src = "images/image5.jpg"; break;
+        case 1: image[0].src = "images/image1.jpg"; break;
+        case 2: image[0].src = "images/image2.jpg"; break;
+        case 3: image[0].src = "images/image3.jpg"; break;
+        case 4: image[0].src = "images/image4.jpg"; break;
+        case 5: image[0].src = "images/image5.jpg"; break;
         default: break;
     }
 
-    var element = document.getElementsByTagName("LI");
+    var element = document.getElementsByTagName("li");
     for (var i = 0; i < element.length; i++) {
-        var subelement = element[i].getElementsByTagName("A");
+        var subelement = element[i].getElementsByTagName("a");
         if (i == link - 1) {
             element[i].style.backgroundColor = 'rgb(0, 150, 140)';
             subelement[0].style.color = 'white';
