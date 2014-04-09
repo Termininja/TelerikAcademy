6116a5f7fed2ae97c53a8db7bdba6d802044bf77
Termininja <slavi.bg@gbg.bg>
2014-04-09 20:37:50 +0300

    CSS

--- Web Design/CSS/2. Presentation/Task 4 - Programmer Calculator/script.js
+++ Web Design/CSS/2. Presentation/Task 4 - Programmer Calculator/script.js
@@ -1,201 +1,383 @@
-﻿var Key = document.Calculator;
-var Keep = 0;
-var Flag = false;
-var Operation = "";
-var binary = 0;
+﻿/// Not work for numbers bigger than 2^53 - 1 = 9 007 199 254 740 991
 
+var keep = 0;
+var memory = 0;
+var flag = false;
+var operator = "";
+var base = 10;
+var binary = Dec2Bin(0);
+var Stack = new Array();
+var Display = Calculator.Display;
+
+// Executed when the page is loaded
 window.onload = function () {
-    Hex(false);
-    OctBin("oct", false);
-    OctBin("bin", false);
-    Bits('dword', true);
-    Bits('word', true);
-    Bits('byte', true);
+    LoadFormat(10);
+    LoadWord('qword');
+    Calculator.Memory.style.visibility = 'hidden';
 };
 
+// Keyboard input detection
+window.onkeyup = function (e) {
+    if (e.shiftKey) {
+        if (e.keyCode == 56) Function('*');
+        if (e.keyCode == 61 || e.keyCode == 187) Function('+');
+        else if (e.keyCode == 57) Parentheses(true);                // Key: ( 
+        else if (e.keyCode == 48) Parentheses(false);               // Key: ) 
+        else if (e.keyCode == 53) Function('mod');                  // Key: % 
+        else if (e.keyCode == 54) Function('xor');                  // Key: ^
+        else if (e.keyCode == 55) Function('and');                  // Key: &
+        else if (e.keyCode == 188) Function('lsh');                 // Key: <
+        else if (e.keyCode == 190) Function('rsh');                 // Key: >
+        else if (e.keyCode == 220) Function('or');                  // Key: | 
+        else if (e.keyCode == 192) Not();                           // Key: ~
+    }
+    else {
+        if (e.keyCode == 8) Back();                                 // Key: ← (backspace)
+        else if (e.keyCode == 27) Clear();                          // Key: Esc
+        else if (e.keyCode == 46) ClearEntry();                     // Key: Delete
+        else if (e.keyCode == 106) Function('*');
+        else if (e.keyCode == 107) Function('+');
+        else if (e.keyCode == 48) Num('0');
+        else if (e.keyCode == 49) Num('1');
+        else if (e.keyCode == 111 || e.keyCode == 191) Function('/');
+        else if (e.keyCode == 109 || e.keyCode == 173 || e.keyCode == 189) Function('-');
+        else if (e.keyCode == 13 || e.keyCode == 61 || e.keyCode == 187) Function('=');
+
+        else if (base == 16 || base == 10 || base == 8) {
+            if (e.keyCode == 50) Num('2');
+            else if (e.keyCode == 51) Num('3');
+            else if (e.keyCode == 52) Num('4');
+            else if (e.keyCode == 53) Num('5');
+            else if (e.keyCode == 54) Num('6');
+            else if (e.keyCode == 55) Num('7');
+
+            else if (base == 16 || base == 10) {
+                if (e.keyCode == 56) Num('8');
+                else if (e.keyCode == 57) Num('9');
+
+                else if (base == 16) {
+                    if (e.keyCode == 65) Num('A');
+                    else if (e.keyCode == 66) Num('B');
+                    else if (e.keyCode == 67) Num('C');
+                    else if (e.keyCode == 68) Num('D');
+                    else if (e.keyCode == 69) Num('E');
+                    else if (e.keyCode == 70) Num('F');
+                }
+            }
+        }
+    }
+}
+
+// Read some input number
 function Num(number) {
-    if (number > 9) {
-        // Do something!
-        alert("HEX!");
+    if (flag) {
+        flag = false;
+        keep = 0;
     }
-    else {
-        if (Flag) {
-            Key.Display.value = number;
-            Flag = false;
-        }
-        else {
-            if (Key.Display.value == "0") Key.Display.value = number;
-            else Key.Display.value += number;
-        }
-        DisplayBits();
+
+    // Check the length of the number
+    var len = Display.value.length;
+    if ((base == 16 && len < 16) || (base == 10 && len < 19) || (base == 8 && len < 22) || (base == 2 && len < 63)) {
+        Display.value = (keep == 0) ? number : Display.value + number;
     }
+
+    Update(parseInt(Display.value, base));
 }
-function Function(value) {
-    var Display = Key.Display.value;
-    if (Flag && Operation != "=");
-    else {
-        Flag = true;
-        if ('+' == Operation) Keep += parseFloat(Display);
-        else if ('-' == Operation) Keep -= parseFloat(Display);
-        else if ('/' == Operation) Keep /= parseFloat(Display);
-        else if ('*' == Operation) Keep *= parseFloat(Display);
-        else Keep = parseFloat(Display);
-        Key.Display.value = Keep;
-        Operation = value;
-    }
-    DisplayBits();
+
+// Functionality for the basic math operations
+function Function(operator) {
+    flag = true;
+
+    if (operator == '+' || operator == '-') priority = 1;
+    else if (operator == '*' || operator == '/' || operator == 'mod') priority = 2;
+    else if (operator == "or" || operator == 'xor') priority = 3;
+    else if (operator == "and") priority = 4;
+    else if (operator == "lsh" || operator == "rsh") priority = 5;
+    else if (operator == '=') while (Stack.length > 0) Pop();
+
+    if (Stack.length > 0 && priority <= Stack[0][2]) Pop();
+    Push(keep, operator, priority);
+
+    Update(keep);
 }
+
+// Parentheses functionality
+function Parentheses(p) {
+    flag = true;
+    if (p) {
+        Push(0, '(', 0);
+        keep = 0;
+    }
+    else while (Pop());
+
+    Update(keep);
+}
+
+// Push some element in the stack
+function Push(keep, operator, priority) {
+    Stack.unshift(new Array(keep, operator, priority));
+}
+
+// Remove some element from the stack
+function Pop() {
+    if (Stack.length == 0) return false;
+
+    // Calculate the result
+    operator = Stack[0][1];
+    var value = Stack[0][0];
+    switch (operator) {
+        case '+': keep = value + keep; break;
+        case '-': keep = value - keep; break;
+        case '*': keep = value * keep; break;
+        case '/': keep = value / keep; break;
+        case 'or': keep = value | keep; break;
+        case 'and': keep = value & keep; break;
+        case 'xor': keep = value ^ keep; break;
+        case 'lsh': keep = value << keep; break;
+        case 'rsh': keep = value >> keep; break;
+        case 'mod': keep = value % keep; break;
+        default: break;
+    }
+    keep = parseInt(keep);
+    Stack.shift();
+
+    return operator != '(';
+}
+
+// Functionality for ClearEntry (CE) button
 function ClearEntry() {
-    Key.Display.value = "0";
-    Flag = true;
-    DisplayBits();
+    flag = true;
+    Update(0);
 }
+
+// Functionality for Clear (C) button
 function Clear() {
-    Keep = 0;
-    Operation = "";
+    operator = null;
+    keep = 0;
+    Stack = new Array();
     ClearEntry();
 }
-function Neg() {
-    Key.Display.value = parseFloat(Key.Display.value) * -1;
-    DisplayBits();
-}
-function Back() {
-    var value = parseFloat(Key.Display.value).toString();
-    if ((Key.Display.value > 0 && value.length > 1) ||
-        (Key.Display.value < 0 && value.length > 2)) {
-        Key.Display.value = parseFloat(value.substring(0, value.length - 1));
-    }
-    else Key.Display.value = 0;
-    DisplayBits();
+
+// Functionality for logical operator NOT
+function Not() {
+    Update(~keep);
 }
 
-function ViewFormat(value) {
+// Bitwise right and left rotations
+function Rotate(direction) {
+    var rotate = (direction == 'r') ? keep >> 1 : keep << 1;
+    Update(rotate);
+}
+
+// Change the sign of the value
+function Neg() {
+    Update(-keep);
+}
+
+// Remove the last char from the value
+function Back() {
+    var len = Display.value.length;
+    var result = ((keep > 0 && len > 1) || (keep < 0 && len > 2)) ?
+        Display.value.substring(0, len - 1) : 0;
+
+    Update(result);
+}
+
+// Memory functionality
+function Mem(action) {
+    var takeMemory = parseInt(Display.value, base);
+    var style = Calculator.Memory.style;
+    switch (action) {
+        case 'MS':
+            // Store the current decimal value in the memory
+            memory = keep;
+            style.visibility = 'visible';
+            break;
+        case 'MR':
+            // Get and display the current memory value
+            takeMemory = memory;
+            flag = false;
+            break;
+        case 'M+':
+            // Add the current value to the stored memory
+            memory += keep;
+            style.visibility = 'visible';
+            break;
+        case 'M-':
+            // Subtract the current value from the stored memory
+            memory -= keep;
+            style.visibility = 'visible';
+            break;
+        default:
+            // Clear all memory
+            memory = 0;
+            style.visibility = 'hidden';
+            break;
+    }
+    Update(takeMemory);
+}
+
+// Change the current numeral system: Hex, Dec, Oct & Bin
+function LoadFormat(value) {
     switch (value) {
-        case 'hex':
-            Hex(true);
+        case 16: Hex(true);
             OctBin("oct", false);
             OctBin("bin", false);
             break;
-        case 'oct':
-            Hex(false);
+        case 8: Hex(false);
             OctBin("oct", true);
             OctBin("bin", false);
             break;
-        case 'bin':
-            Hex(false);
+        case 2: Hex(false);
             OctBin("oct", true);
             OctBin("bin", true);
-            var bynaryText = binary.replace(/^0+/, '');
-            if (bynaryText.length > 12) {
-                Key.Display.style.fontSize = "11px";
-                Key.Display.style.paddingTop = "32px";
-                Key.Display.style.height = "18px";
-                Key.Display.value = bynaryText;
-            }
             break;
-        default:
-            Hex(false);
+        default: Hex(false);
             OctBin("oct", false);
             OctBin("bin", false);
             break;
     }
+
+    Display.value = keep.toString(value).toUpperCase();
+    CheckValueLength();
+    base = value;
+
+    // Configuration for hexadecimal numeral system
+    function Hex(show) {
+        var elements = document.getElementsByClassName('blocked transition1');
+        for (var i = 0; i < elements.length; ++i) {
+            elements[i].style.visibility = show ? 'visible' : 'hidden';
+        }
+
+        if (show) {
+            var elements = document.getElementsByClassName('blocked letter');
+            while (elements.length > 0) {
+                elements[0].disabled = false;
+                elements[0].className = elements[0].className.replace(/\bblocked\b/, "");
+            }
+        }
+        else {
+            var elements = document.getElementsByClassName('letter');
+            for (var i = 0; i < elements.length; ++i) {
+                elements[i].setAttribute("class", "blocked letter");
+                elements[i].disabled = true;
+            }
+        }
+    }
+
+    // Configuration for octal and binary numeral systems
+    function OctBin(mode, hide) {
+        var elements = document.getElementsByClassName(mode + " transition1");
+        for (var i = 0; i < elements.length; ++i) {
+            elements[i].style.visibility = hide ? 'hidden' : 'visible';
+        }
+
+        if (hide) {
+            var elements = document.getElementsByClassName("blocked " + mode + " number");
+            while (elements.length > 0) {
+                elements[0].disabled = true;
+                elements[0].className = elements[0].className.replace(/\bnumber\b/, "");
+            }
+        }
+        else {
+            var elements = document.getElementsByClassName("blocked " + mode);
+            for (var i = 0; i < elements.length; ++i) {
+                elements[i].setAttribute("class", "blocked " + mode + " number");
+                elements[i].disabled = false;
+            }
+        }
+    }
 }
-function ViewBits(value) {
+
+// Change the current bit entry word: Qword, Dword, Word & Byte
+function LoadWord(value) {
+    var bitLimit;
     switch (value) {
-        case 'dword':
-            Bits('dword', false);
-            Bits('word', true);
-            Bits('byte', true);
+        case 'dword': bitLimit = 32;
+            Word('dword', false);
+            Word('word', true);
+            Word('byte', true);
             break;
-        case 'word':
-            Bits('dword', false);
-            Bits('word', false);
-            Bits('byte', true);
+        case 'word': bitLimit = 48;
+            Word('dword', false);
+            Word('word', false);
+            Word('byte', true);
             break;
-        case 'byte':
-            Bits('dword', false);
-            Bits('word', false);
-            Bits('byte', false);
+        case 'byte': bitLimit = 56;
+            Word('dword', false);
+            Word('word', false);
+            Word('byte', false);
             break;
         default:
-            Bits('dword', true);
-            Bits('word', true);
-            Bits('byte', true);
+            Word('dword', true);
+            Word('word', true);
+            Word('byte', true);
             break;
     }
-}
-function Hex(show) {
-    if (show) {
-        var elements = document.getElementsByClassName('blocked letter');
-        while (elements.length > 0) {
-            elements[0].disabled = false;
-            elements[0].className = elements[0].className.replace(/\bblocked\b/, "");
-        }
 
-        var elements2 = document.getElementsByClassName('blocked transition1');
-        for (var i = 0; i < elements2.length; ++i) {
-            elements2[i].style.visibility = 'visible';
+    // Configuration for bit entry word
+    function Word(word, show) {
+        var element = document.getElementsByClassName(word);
+        for (var i = 0; i < element.length; i++) {
+            element[i].style.visibility = show ? 'visible' : 'hidden';
         }
     }
-    else {
-        var elements = document.getElementsByClassName('letter');
-        for (var i = 0; i < elements.length; ++i) {
-            elements[i].setAttribute("class", "blocked letter");
-            elements[i].disabled = true;
-        }
 
-        var elements2 = document.getElementsByClassName('blocked transition1');
-        for (var i = 0; i < elements2.length; ++i) {
-            elements2[i].style.visibility = 'hidden';
-        }
-    }
+    // Correct the value when bit word is changed
+    binary = binary.toString().substr(bitLimit);
+
+    Update(parseInt(binary, 2));
 }
-function OctBin(mode, hide) {
-    if (hide) {
-        var elements = document.getElementsByClassName("blocked " + mode + " number");
-        while (elements.length > 0) {
-            elements[0].disabled = true;
-            elements[0].className = elements[0].className.replace(/\bnumber\b/, "");
-        }
-        var elements2 = document.getElementsByClassName(mode + " transition1");
-        for (var i = 0; i < elements2.length; ++i) {
-            elements2[i].style.visibility = 'hidden';
-        }
-    }
-    else {
-        var elements = document.getElementsByClassName("blocked " + mode);
-        for (var i = 0; i < elements.length; ++i) {
-            elements[i].setAttribute("class", "blocked " + mode + " number");
-            elements[i].disabled = false;
-        }
-        var elements2 = document.getElementsByClassName(mode + " transition1");
-        for (var i = 0; i < elements2.length; ++i) {
-            elements2[i].style.visibility = 'visible';
-        }
-    }
-}
-function Bits(word, show) {
-    var element = document.getElementsByClassName(word);
-    for (var i = 0; i < element.length; i++) {
-        element[i].style.visibility = show ? 'visible' : 'hidden';
+
+// Check the length of the current value
+function CheckValueLength() {
+    var len = Display.value.length;
+    if (len > 50) Style("11px", "32px", "18px");
+    else if (len > 42) Style("14px", "28px", "22px");
+    else if (len > 34) Style("17px", "24px", "26px");
+    else if (len > 26) Style("20px", "20px", "30px");
+    else Style("26px", "10px", "40px");
+
+    // Change the font style for some element
+    function Style(font, top, height) {
+        Display.style.fontSize = font;
+        Display.style.paddingTop = top;
+        Display.style.height = height;
     }
 }
 
-function DisplayBits() {
-    var inputs = document.getElementsByTagName("input");
-
-    var n = parseInt((Key.Display.value).toString());
-    binary = Dec2Bin(n);
-
-    for (var i = 0; i < binary.length; i++) {
-        document.getElementsByName("Bit" + i)[0].value = binary[63 - i];
-    }
-}
-
+// Convert some decimal number to binary (64bit)
 function Dec2Bin(int) {
     var bin;
     if (int >= 0) bin = int.toString(2);
     else bin = (-int - 1).toString(2).replace(/[01]/g, function (d) { return +!+d; });
+
     while (bin.length < 64) bin = ((int >= 0) ? "0" : "1") + bin;
     return bin;
+}
+
+// Change some specific bit from the value
+function ChangeBit(bit) {
+    binary =
+        binary.toString().substr(0, 63 - bit) +
+        ((binary[63 - bit] == '1') ? '0' : '1') +
+        binary.toString().substr(64 - bit);
+
+    Update(parseInt(binary, 2));
+}
+
+// Update the current status of the calculator
+function Update(value) {
+    Display.value = value.toString(base).toUpperCase();
+    keep = parseInt(Display.value, base);
+    CheckValueLength();
+
+    // Remove the focus from the last clicked element
+    document.activeElement.blur();
+
+    // Prints binary representation of any number
+    binary = Dec2Bin(keep);
+    for (var i = 0; i < 64; i++) {
+        document.getElementById("Bit" + i).innerHTML = binary[63 - i];
+    }
 }
--- Web Design/CSS/3. Layout/Task 6 - BTV Page/script.js
+++ Web Design/CSS/3. Layout/Task 6 - BTV Page/script.js
@@ -1,28 +1,55 @@
+// The current page
+var selectedPage = 2;
+
+// Executed when page is loaded 
 window.onload = function () {
-    MediaLink(1);
+    DisplayPage('1');
 }
 
-function MediaLink(link) {
-    var image = document.getElementsByTagName('img');
-    switch (link) {
-        case 1: image[0].src = "images/image1.jpg"; break;
-        case 2: image[0].src = "images/image2.jpg"; break;
-        case 3: image[0].src = "images/image3.jpg"; break;
-        case 4: image[0].src = "images/image4.jpg"; break;
-        case 5: image[0].src = "images/image5.jpg"; break;
-        default: break;
+// Page Navigator
+function DisplayPage(page) {
+    selectedPage = page;
+    var pages = document.getElementsByTagName('aside');
+    for (var i = 1; i <= pages.length; i++) {
+
+        // Tab styles in navigation bar
+        var isSelected = i == page;
+        var navElement = document.getElementById('nav' + i);
+        navElement.style.background = isSelected ? 'transparent' : 'rgb(75, 95, 103)';
+        navElement.style.color = isSelected ? 'white' : 'rgb(147, 172, 182)';
+        navElement.style.cursor = isSelected ? 'default' : 'pointer';
+        if (isSelected) navElement.style.textDecoration = 'none';
+
+        // Display selected page and hide all others
+        pages[i - 1].style.visibility =
+            (pages[i - 1].id.substring(pages.length - 2) == page) ? 'visible' : 'hidden';
     }
 
+    // The first link of the current page will be selected
+    MediaLink(5 * selectedPage - 4);
+}
+
+// On mouse over functionality for navigation bar
+function OnMouseOver(nav, action) {
+    if (selectedPage != nav) {
+        var element = document.getElementById('nav' + nav);
+        element.style.textDecoration = action ? 'underline' : 'none';
+    }
+}
+
+// Link functionality for each page
+function MediaLink(n) {
+
+    // Image of the link
+    var images = document.getElementsByTagName('img');
+    images[0].src = 'images/image' + n + '.jpg';
+
+    // Link styles in navigation menu
     var element = document.getElementsByTagName("li");
-    for (var i = 0; i < element.length; i++) {
-        var subelement = element[i].getElementsByTagName("a");
-        if (i == link - 1) {
-            element[i].style.backgroundColor = 'rgb(0, 150, 140)';
-            subelement[0].style.color = 'white';
-        }
-        else {
-            element[i].style.backgroundColor = 'transparent';
-            subelement[0].style.color = 'rgb(165, 175, 179)';
-        }
+    var lastLinkNumber = 5 * selectedPage;
+    for (var i = lastLinkNumber - 4; i <= 5 * selectedPage; i++) {
+        var subelement = element[i - 1].getElementsByTagName("a");
+        subelement[0].style.color = (i == n) ? 'white' : 'rgb(165, 175, 179)';
+        element[i - 1].style.backgroundColor = (i == n) ? 'rgb(0, 150, 140)' : 'transparent';
     }
 }
