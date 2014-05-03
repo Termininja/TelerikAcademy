a1198cdfbcb2099701b3ab584b12ec6f8890448d
Termininja <slavi.bg@gbg.bg>
2014-05-03 11:40:45 +0300

    JS

---
+++ Web Design/JavaScript/BGCoder/Max Sum/Max Sum.js
@@ -0,0 +1,24 @@
+function Solve(params) {
+    var N = parseInt(params[0]);
+
+    var arr = [];
+    for (var i = 0; i < params.length; i++) {
+        arr.push(parseInt(params[i]));
+    }
+
+    var maxSum = Number.NEGATIVE_INFINITY;
+    for (var i = 1; i <= N; i++) {
+        for (var j = i; j <= N; j++) {
+            var sum = 0;
+            for (var p = i; p <= j; p++) {
+                sum += arr[p];
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+
+    return maxSum;
+}
