aef674fb552f75d55a4c3368e6896d8b7169bb57
Termininja <slavi.bg@gbg.bg>
2014-05-19 19:24:58 +0300

    HQC

---
+++ High-Quality Code/Homework/02. Naming Identifiers/Person/Person.cs
@@ -0,0 +1,38 @@
+﻿//Task 2. Refactor the following examples to produce code with well-named identifiers in C#:
+
+using System;
+
+class Program
+{
+    enum Sex { Man, Woman };
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Sex Sex { get; set; }
+    }
+
+    private static Person CreatePerson(int age)
+    {
+        Person person = new Person();
+        person.Age = age;
+        if (age % 2 == 0)
+        {
+            person.Name = "Батката";
+            person.Sex = Sex.Man;
+        }
+        else
+        {
+            person.Name = "Мацето";
+            person.Sex = Sex.Woman;
+        }
+
+        return person;
+    }
+
+    static void Main()
+    {
+        Person newPerson = CreatePerson(22);
+    }
+}
---
+++ High-Quality Code/Homework/02. Naming Identifiers/Printer/Printer.cs
@@ -0,0 +1,22 @@
+﻿//Task 1. Refactor the following examples to produce code with well-named C# identifiers:
+
+using System;
+
+class Printer
+{
+    private const int MaxCount = 6;
+
+    class Print
+    {
+        public void ToString(bool variable)
+        {
+            Console.WriteLine(variable.ToString());
+        }
+    }
+
+    static void Main()
+    {
+        Print printer = new Print();
+        printer.ToString(true);
+    }
+}
