cab12f929f808117c526c39d43d1051c2f37e129
Termininja <slavi.bg@gbg.bg>
2014-06-14 10:33:02 +0300

    JS

---
+++ JavaScript/JS UI & DOM/06. Canvas Animations/Readme.md
@@ -0,0 +1,25 @@
+## Canvas Animations
+
+**Task 01.** Create a walking Super Mario.
+ * Super Mario must be walking indefinitely from left to right on the screen
+ * The background must be created using SVG
+ * Additional requirements:
+  * You can use sprites from [this link](https://www.google.com/search?q=super+mario+sprite&newwindow=1&es_sm=122&source=lnms&tbm=isch&sa=X&ei=4haQU4mKJKjB0QXBo4GIBw&ved=0CAgQ_AUoAQ&biw=1618&bih=965)
+  * Use Canvas for Super Mario
+  * Use SVG for the background
+  * You can use Raphael and/or KineticJS, or native Canvas/SVG APIs
+
+**Task 02.** Finish the aircraft game from the demo.
+ * Create flying monsters that can kill the fighter.
+ * The fighter can kill the monsters.
+ * When the fighter is destroyed, a high score is calculated based on the time he was alive and the number of monsters killed.
+
+**Task 03.** Create the famous game Falling Rocks (Padashti kamani).
+ * The game consists of a dwarf at the bottom of the field.
+  * The dwarf can move left and right.
+ * Rocks are randomly generated at the top of the field.
+  * The rocks can kill the dwarf.
+ * The dwarf must evade the rocks as long as possible.
+
+**Task 04\*.** Create a ball that is moving in a spiral trajectory, starting from the middle of the drawing field.
+ * Hint: find the mathematical formula for the ***Archimedean spiral*** to calculate the trajectory of the ball.
---
+++ JavaScript/JS UI & DOM/06. Canvas Animations/scripts/task-01-super-mario.js
@@ -0,0 +1,96 @@
+﻿var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var backgroundMusic = loadSound('sounds/theme.mp3');
+var jumpSound = loadSound("sounds/jump.mp3");
+var crouchSound = loadSound("sounds/crouch.mp3");
+var lastKeyPressed = 0;
+var step = 20;
+
+var mario = { x: 350, y: 320, image: 'images/mario.png' };
+
+var img = new Image();
+img.src = mario.image;
+
+window.onload = function () {
+    draw();
+    backgroundMusic.play();
+    action();
+};
+
+function action() {
+    document.onkeydown = function (e) {
+        ctx.clearRect(0, 0, 800, 500);
+        if (e.keyCode == 38) jump();
+        else if (e.keyCode == 37 && mario.x > 0) left();
+        else if (e.keyCode == 39 && mario.x < 670) right();
+        else if (e.keyCode == 40) crouch();
+
+        img.src = mario.image;
+        setInterval(function () { draw() }, 3);
+    }
+
+    document.onkeyup = function myfunction() {
+        ctx.clearRect(0, 0, 800, 500);
+        mario.image = 'images/mario.png';
+        img.src = mario.image;
+        setInterval(function () { draw() }, 3);
+    }
+
+    function jump() {
+        mario.image = 'images/mario.png';
+        var call = setInterval(function () {
+            ctx.clearRect(0, 0, 800, 500);
+            mario.y -= step;
+            draw();
+            if (mario.y < 150) {
+                step = -20;
+            }
+            if (mario.y >= 320) {
+                clearInterval(call);
+                step = 20;
+            }
+        }, 30);
+        jumpSound.play();
+    }
+
+    function left() {
+        if (lastKeyPressed == 37) {
+            mario.image = 'images/left2.png';
+            lastKeyPressed = 0;
+        }
+        else if (lastKeyPressed == 0 || lastKeyPressed == 39 || lastKeyPressed == 38 || lastKeyPressed == 40) {
+            mario.image = 'images/left1.png';
+            lastKeyPressed = 37;
+        }
+        mario.x -= step;
+    }
+
+    function right() {
+        if (lastKeyPressed == 39) {
+            mario.image = 'images/right2.png';
+            lastKeyPressed = 0;
+        }
+        else if (lastKeyPressed == 0 || lastKeyPressed == 37 || lastKeyPressed == 38 || lastKeyPressed == 40) {
+            mario.image = 'images/right1.png';
+            lastKeyPressed = 39;
+        }
+        mario.x += step;
+    }
+
+    function crouch() {
+        mario.image = 'images/crouch.png';
+        lastKeyPressed = 40;
+    }
+}
+
+function draw() {
+    ctx.drawImage(img, mario.x, mario.y);
+}
+
+function loadSound(url) {
+    var audio = new Audio();
+    audio.preload = "auto";
+    audio.src = url;
+    audio.load();
+    return audio;
+}
