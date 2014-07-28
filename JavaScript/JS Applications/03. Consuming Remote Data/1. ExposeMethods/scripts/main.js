c1b50166a2c5103c3a87cadcd7e34b5f4e077d4b
Termininja <slavi.bg@gbg.bg>
2014-07-29 00:06:48 +0300

    JS

---
+++ JavaScript/JS Applications/03. Consuming Remote Data/1. ExposeMethods/scripts/main.js
@@ -0,0 +1,20 @@
+﻿define(function () {
+    require.config({
+        paths: {
+            'jquery': 'libs/jquery-2.0.2',
+            'q-promise': 'libs/q-promise',
+        }
+    });
+
+    require(['script'], function (HttpRequest) {
+        var url = 'http://crowd-chat.herokuapp.com/posts';
+
+        HttpRequest.postJSON(url, { user: 'Ivan', text: 'Hello' }).then(function (data) {
+            console.log(data);
+        });
+
+        HttpRequest.getJSON(url).then(function (data) {
+            console.log(data[data.length - 1]);
+        });
+    });
+});
