29b8c8ce32326a7b164ce9a92ea314b5d8aa1508
Termininja <slavi.bg@gbg.bg>
2014-08-25 14:06:47 +0300

    DSA

---
+++ DSA/01. Data Structures, Algorithms and Complexity/CalculateCount/CalculateCount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class CalculateCount
+{
+    static long complexity = 0;
+
+    static void Main()
+    {
+        // worst case matrix
+        int[,] matrix = new int[,] { 
+                {2, 5, 7, 6, 4, 2, 3, 4, 7, 5, 9, 7, 3, 5, 6, 7, 6, 4, 6, 2},
+                {4, 8, 1, 2, 4, 2, 1 ,4 ,6 ,7, 9, 3, 9, 6, 6, 6, 3, 5, 1, 2},
+                {8, 7, 9, 6, 2, 1, 1, 5, 9, 6, 8, 3, 2, 4, 5, 2, 8, 7, 2, 1},
+        };
+        int n = matrix.GetLength(0);
+        int m = matrix.GetLength(1);
+        CalcCount(matrix);
+
+        Console.WriteLine("n = {0}\nm = {1}", n, m);
+        Console.WriteLine("\nIn the worst case the complexity is: O(n*m)=O({0})", n * m);
+        Console.WriteLine("In the best case the complexity is: O(n)=O({0})", n);
+        Console.WriteLine("\nComplexity from the test in the worst case: " + complexity);
+    }
+
+    private static long CalcCount(int[,] matrix)
+    {
+        long count = 0;
+
+        // repeated n times (n = matrix.GetLength(0))
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            // repeated n times if the first element in the row is even
+            if (matrix[row, 0] % 2 == 0)
+            {
+                // repeated m times (m = matrix.GetLength(1)) for each n
+                // or n*m times in the worst case
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    // repeated n*m times if the element is positive
+                    if (matrix[row, col] > 0)
+                    {
+                        count++;
+                        complexity++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ DSA/01. Data Structures, Algorithms and Complexity/Calculation/Calculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Calculation
+{
+    static long complexity = 0;
+
+    static void Main()
+    {
+        int[] arr = new int[] { 9, 4, 5, 7, 8, 9, 3, 4, 6, 3 };
+        int n = arr.Length; 
+        Compute(arr);
+
+        Console.WriteLine("n = {0}", n);
+        Console.WriteLine("\nThe final complexity is: n*(n-1) = O({0})", n * (n - 1));
+        Console.WriteLine("\nComplexity from the test: " + complexity);
+    }
+
+    private static long Compute(int[] arr)
+    {
+        long count = 0;
+
+        // repeated n times (n = arr.Length)
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            // repeated n-1 times (n-1 = end) for each n from the previous loop: n*(n-1)
+            while (start < end)
+            {
+                if (arr[start] < arr[end])
+                {
+                    start++; count++;
+                }
+                else
+                {
+                    end--;
+                }
+
+                complexity++;
+            }
+        }
+
+        return count;
+    }
+}
