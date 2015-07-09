ba347a510150a1d2857d20dab2ebd92b29d14599
unknown <termininja@mail.bg>
2015-07-09 23:48:09 +0300

    High-Quality Code

--- HQC/03. Code Documentation & Comments/StringExtensions/StringExtensions.cs
+++ HQC/03. Code Documentation & Comments/StringExtensions/StringExtensions.cs
@@ -51,6 +51,7 @@ namespace Telerik.ILS.Common
         public static bool ToBoolean(this string input)
         {
             var stringTrueValues = new[] { "true", "ok", "yes", "1", "да" };
+
             return stringTrueValues.Contains(input.ToLower());
         }
 
@@ -63,6 +64,7 @@ namespace Telerik.ILS.Common
         {
             short shortValue;
             short.TryParse(input, out shortValue);
+
             return shortValue;
         }
 
@@ -75,6 +77,7 @@ namespace Telerik.ILS.Common
         {
             int integerValue;
             int.TryParse(input, out integerValue);
+
             return integerValue;
         }
 
@@ -87,6 +90,7 @@ namespace Telerik.ILS.Common
         {
             long longValue;
             long.TryParse(input, out longValue);
+
             return longValue;
         }
 
@@ -94,11 +98,12 @@ namespace Telerik.ILS.Common
         /// Parse some input string as a DateTime.
         /// </summary>
         /// <param name="input">Some input string.</param>
-        /// <returns>Returns a string value as DataTime</returns>
+        /// <returns>Returns a string value as DataTime.</returns>
         public static DateTime ToDateTime(this string input)
         {
             DateTime dateTimeValue;
             DateTime.TryParse(input, out dateTimeValue);
+
             return dateTimeValue;
         }
 
@@ -114,7 +119,7 @@ namespace Telerik.ILS.Common
                 return input;
             }
 
-            return input.Substring(0, 1).ToUpper(CultureInfo.CurrentCulture) + input.Substring(1, input.Length - 1);
+            return input.Substring(0, 1).ToUpper(CultureInfo.CurrentCulture) + input.Substring(1);
         }
 
         /// <summary>
@@ -129,24 +134,20 @@ namespace Telerik.ILS.Common
         {
             input = input.Substring(startFrom);
             startFrom = 0;
-            if (!input.Contains(startString) || !input.Contains(endString))
+            if (input.Contains(startString) && input.Contains(endString))
             {
-                return string.Empty;
+                var startPosition = input.IndexOf(startString, startFrom, StringComparison.Ordinal) + startString.Length;
+                if (startPosition != -1)
+                {
+                    var endPosition = input.IndexOf(endString, startPosition, StringComparison.Ordinal);
+                    if (endPosition != -1)
+                    {
+                        return input.Substring(startPosition, endPosition - startPosition);
+                    }
+                }
             }
 
-            var startPosition = input.IndexOf(startString, startFrom, StringComparison.Ordinal) + startString.Length;
-            if (startPosition == -1)
-            {
-                return string.Empty;
-            }
-
-            var endPosition = input.IndexOf(endString, startPosition, StringComparison.Ordinal);
-            if (endPosition == -1)
-            {
-                return string.Empty;
-            }
-
-            return input.Substring(startPosition, endPosition - startPosition);
+            return string.Empty;
         }
 
         /// <summary>
@@ -158,14 +159,16 @@ namespace Telerik.ILS.Common
         {
             var bulgarianLetters = new[]
             {
-                "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п",
-                "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я"
+                "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
+                "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я"
             };
+
             var latinRepresentationsOfBulgarianLetters = new[]
             {
                 "a", "b", "v", "g", "d", "e", "j", "z", "i", "y", "k","l", "m", "n", "o", "p",
                 "r", "s", "t", "u", "f", "h", "c", "ch", "sh", "sht", "u", "i", "yu", "ya"
             };
+
             for (var i = 0; i < bulgarianLetters.Length; i++)
             {
                 input = input.Replace(bulgarianLetters[i], latinRepresentationsOfBulgarianLetters[i]);
@@ -187,6 +190,7 @@ namespace Telerik.ILS.Common
                 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
             };
+
             var bulgarianRepresentationOfLatinKeyboard = new[]
             {
                 "а", "б", "ц", "д", "е", "ф", "г", "х", "и", "й", "к", "л", "м", 
@@ -210,7 +214,8 @@ namespace Telerik.ILS.Common
         public static string ToValidUsername(this string input)
         {
             input = input.ConvertCyrillicToLatinLetters();
-            return Regex.Replace(input, @"[^a-zA-z0-9_\.]+", string.Empty);
+
+            return Regex.Replace(input, @"[^A-z\d_\.]+", string.Empty);
         }
 
         /// <summary>
@@ -221,7 +226,8 @@ namespace Telerik.ILS.Common
         public static string ToValidLatinFileName(this string input)
         {
             input = input.Replace(" ", "-").ConvertCyrillicToLatinLetters();
-            return Regex.Replace(input, @"[^a-zA-z0-9_\.\-]+", string.Empty);
+
+            return Regex.Replace(input, @"[^A-z\d_\.\-]+", string.Empty);
         }
 
         /// <summary>
@@ -242,18 +248,16 @@ namespace Telerik.ILS.Common
         /// <returns>Returns the file extension.</returns>
         public static string GetFileExtension(this string fileName)
         {
-            if (string.IsNullOrWhiteSpace(fileName))
+            if (!string.IsNullOrWhiteSpace(fileName))
             {
-                return string.Empty;
+                var fileParts = fileName.Split('.');
+                if (fileParts.Count() != 1 && !string.IsNullOrEmpty(fileParts.Last()))
+                {
+                    return fileParts.Last().Trim().ToLower();
+                }
             }
 
-            string[] fileParts = fileName.Split(new[] { "." }, StringSplitOptions.None);
-            if (fileParts.Count() == 1 || string.IsNullOrEmpty(fileParts.Last()))
-            {
-                return string.Empty;
-            }
-
-            return fileParts.Last().Trim().ToLower();
+            return string.Empty;
         }
 
         /// <summary>
@@ -263,17 +267,16 @@ namespace Telerik.ILS.Common
         /// <returns>Returns the content type of given file extension.</returns>
         public static string ToContentType(this string fileExtension)
         {
-            var fileExtensionToContentType = new Dictionary<string, string>
-            {
-                { "jpg", "image/jpeg" },
-                { "jpeg", "image/jpeg" },
-                { "png", "image/x-png" },
-                { "docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
-                { "doc", "application/msword" },
-                { "pdf", "application/pdf" },
-                { "txt", "text/plain" },
-                { "rtf", "application/rtf" }
-            };
+            var fileExtensionToContentType = new Dictionary<string, string>();
+            fileExtensionToContentType["jpg"] = "image/jpeg";
+            fileExtensionToContentType["jpeg"] = "image/jpeg";
+            fileExtensionToContentType["png"] = "image/x-png";
+            fileExtensionToContentType["docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+            fileExtensionToContentType["doc"] = "application/msword";
+            fileExtensionToContentType["pdf"] = "application/pdf";
+            fileExtensionToContentType["txt"] = "text/plain";
+            fileExtensionToContentType["rtf"] = "application/rtf";
+
             if (fileExtensionToContentType.ContainsKey(fileExtension.Trim()))
             {
                 return fileExtensionToContentType[fileExtension.Trim()];
@@ -291,6 +294,7 @@ namespace Telerik.ILS.Common
         {
             var bytesArray = new byte[input.Length * sizeof(char)];
             Buffer.BlockCopy(input.ToCharArray(), 0, bytesArray, 0, bytesArray.Length);
+
             return bytesArray;
         }
     }
